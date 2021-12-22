b8d088a3245a0ecf447779b75ea540ce5b026d36
huangjiewei <756898445@qq.com>
2021-12-22 16:11:39 +0800

    ddt

--- test_cases/test_case_for_search.py
+++ test_cases/test_case_for_search.py
@@ -4,8 +4,9 @@ from selenium import webdriver
 
 from page_object.index_page import IndexPage
 from page_object.login_page import LoginPage
+from ddt import ddt,file_data
 
-
+@ddt
 class TestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -17,19 +18,21 @@ class TestCase(unittest.TestCase):
     def tearDownClass(cls) -> None:
         cls.driver.quit()
 
-    def test_1_login(self):
-        #测试数据
-        username = '756898445@qq.com'
-        password = 'LTXWJwyyjk123'
-        txt='123'
+    @file_data('../data/user.yaml')
+    def test_1_login(self,username,password):
+
 
         #测试流程
         self.lp.login(username, password)
         time.sleep(2)
+
+    def test_2_search(self):
+        # 测试数据
+        txt = '456'
+
+        # 测试流程
         self.ip.search(txt)
         time.sleep(2)
-        #driver.quit()
-
 
 
 if __name__ == '__main__':
