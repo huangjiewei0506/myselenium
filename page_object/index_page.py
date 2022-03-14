e8119656a7892a2eac9010d9f8405ae7469e0905
huangjiewei <756898445@qq.com>
2022-03-14 19:03:43 +0800

    增加注释

--- page_object/index_page.py
+++ page_object/index_page.py
@@ -11,6 +11,7 @@ class IndexPage(BasePage):
      #核心元素
     url="https://lanhuapp.com/web/#/item"
 
+    #首页搜索按钮，关键字输入框
     search_button=(By.CLASS_NAME,'serch_icon')
     input_button=(By.XPATH,"//div[@class='search on']//input[@type='text']")
     makesure_search=(By.XPATH,"//div[@class='search_btn']")
